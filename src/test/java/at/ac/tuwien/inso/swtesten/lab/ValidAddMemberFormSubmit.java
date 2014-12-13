cd330bfc704772e031511c12ae9ef8ec5eeab062
Thomas Schmidleithner <thomas.schmidleithner@gmail.com>
2014-12-13 15:53:18 +0100

    format code

--- src/test/java/at/ac/tuwien/inso/swtesten/lab/ValidAddMemberFormSubmit.java
+++ src/test/java/at/ac/tuwien/inso/swtesten/lab/ValidAddMemberFormSubmit.java
@@ -1,7 +1,5 @@
 package at.ac.tuwien.inso.swtesten.lab;
 
-import static org.junit.Assert.assertEquals;
-
 import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
 
@@ -13,101 +11,108 @@ import org.openqa.selenium.WebDriver;
 import at.ac.tuwien.inso.swtesten.util.SeleniumWebDriver;
 
 public class ValidAddMemberFormSubmit {
-  private WebDriver driver;
-  private String baseUrl;
-  private boolean acceptNextAlert = true;
-  private StringBuffer verificationErrors = new StringBuffer();
+	private WebDriver driver;
+	private String baseUrl;
+	private boolean acceptNextAlert = true;
+	private StringBuffer verificationErrors = new StringBuffer();
 
-  public void setUp() throws Exception {
-    driver = SeleniumWebDriver.getCurrentDriver();
-    baseUrl = "https://sportsid.risedev.at/";
-    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-  }
-  
-  public void login(){
-	driver.get(baseUrl + "/#pages.login");
-	driver.findElement(By.id("input-username")).clear();
-	driver.findElement(By.id("input-username")).sendKeys("gruppe04a");
-	driver.findElement(By.id("input-password")).clear();
-	driver.findElement(By.id("input-password")).sendKeys("1234");
-	driver.findElement(By.id("login-button")).click();
-  }
+	public void setUp() throws Exception {
+		driver = SeleniumWebDriver.getCurrentDriver();
+		baseUrl = "https://sportsid.risedev.at/";
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
 
-  public void createUser(String name) {
-	  
-	  login();
-	
-	  driver.findElement(By.id("nav-members")).click();
-    driver.findElement(By.id("nav-members-list")).click();
-    
-    driver.findElement(By.cssSelector("#new-member > span.ui-button-text > span.button-content")).click();
-    
-    driver.findElement(By.id("member-family-name-native")).clear();
-    driver.findElement(By.id("member-family-name-native")).sendKeys("MAX");
-    driver.findElement(By.id("member-first-name-native")).clear();
-    driver.findElement(By.id("member-first-name-native")).sendKeys(name);
-    driver.findElement(By.cssSelector("button.ui-datepicker-trigger")).click();
-    driver.findElement(By.linkText("8")).click();
-    driver.findElement(By.xpath("(//button[@type='button'])[2]")).click();
-    driver.findElement(By.id("ui-id-9")).click();
-    driver.findElement(By.id("member-gender-m")).click();
-    driver.findElement(By.id("member-family-name")).clear();
-    driver.findElement(By.id("member-family-name")).sendKeys("MXILEIT");
-    driver.findElement(By.id("member-first-name")).clear();
-    driver.findElement(By.id("member-first-name")).sendKeys("WASAUCHUMMER");
-    driver.findElement(By.cssSelector("#save-member > span.ui-button-text > span.button-content")).click();
-  }
+	public void login() {
+		driver.get(baseUrl + "/#pages.login");
+		driver.findElement(By.id("input-username")).clear();
+		driver.findElement(By.id("input-username")).sendKeys("gruppe04a");
+		driver.findElement(By.id("input-password")).clear();
+		driver.findElement(By.id("input-password")).sendKeys("1234");
+		driver.findElement(By.id("login-button")).click();
+	}
 
-  private boolean isElementPresent(By by) {
-    try {
-      driver.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
-    }
-  }
+	public void createUser(String name) {
 
-  private boolean isAlertPresent() {
-    try {
-      driver.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
-  }
+		login();
 
-  private String closeAlertAndGetItsText() {
-    try {
-      Alert alert = driver.switchTo().alert();
-      String alertText = alert.getText();
-      if (acceptNextAlert) {
-        alert.accept();
-      } else {
-        alert.dismiss();
-      }
-      return alertText;
-    } finally {
-      acceptNextAlert = true;
-    }
-  }
+		driver.findElement(By.id("nav-members")).click();
+		driver.findElement(By.id("nav-members-list")).click();
 
-public void hasUser(String arg1) {
-	// TODO Auto-generated method stub
-	//driver.findElement(By.id());
-}
+		driver.findElement(
+				By.cssSelector("#new-member > span.ui-button-text > span.button-content"))
+				.click();
 
-public void createLocalOrganisation(String arg1) {
-	// TODO Auto-generated method stub
-	login();
-	
-	driver.findElement(By.id("nav-structure")).click();
-    driver.findElement(By.id("nav-structure-list-local")).click();
-    driver.findElement(By.cssSelector("span.button-content")).click();
-    
-    driver.findElement(By.id("group-name")).clear();
-    driver.findElement(By.id("group-name")).sendKeys(arg1);
-    
-    driver.findElement(By.cssSelector("#save-localorg > span.ui-button-text > span.button-content")).click();
+		driver.findElement(By.id("member-family-name-native")).clear();
+		driver.findElement(By.id("member-family-name-native")).sendKeys("MAX");
+		driver.findElement(By.id("member-first-name-native")).clear();
+		driver.findElement(By.id("member-first-name-native")).sendKeys(name);
+		driver.findElement(By.cssSelector("button.ui-datepicker-trigger"))
+				.click();
+		driver.findElement(By.linkText("8")).click();
+		driver.findElement(By.xpath("(//button[@type='button'])[2]")).click();
+		driver.findElement(By.id("ui-id-9")).click();
+		driver.findElement(By.id("member-gender-m")).click();
+		driver.findElement(By.id("member-family-name")).clear();
+		driver.findElement(By.id("member-family-name")).sendKeys("MXILEIT");
+		driver.findElement(By.id("member-first-name")).clear();
+		driver.findElement(By.id("member-first-name")).sendKeys("WASAUCHUMMER");
+		driver.findElement(
+				By.cssSelector("#save-member > span.ui-button-text > span.button-content"))
+				.click();
+	}
 
-}
+	private boolean isElementPresent(By by) {
+		try {
+			driver.findElement(by);
+			return true;
+		} catch (NoSuchElementException e) {
+			return false;
+		}
+	}
+
+	private boolean isAlertPresent() {
+		try {
+			driver.switchTo().alert();
+			return true;
+		} catch (NoAlertPresentException e) {
+			return false;
+		}
+	}
+
+	private String closeAlertAndGetItsText() {
+		try {
+			Alert alert = driver.switchTo().alert();
+			String alertText = alert.getText();
+			if (acceptNextAlert) {
+				alert.accept();
+			} else {
+				alert.dismiss();
+			}
+			return alertText;
+		} finally {
+			acceptNextAlert = true;
+		}
+	}
+
+	public void hasUser(String arg1) {
+		// TODO Auto-generated method stub
+		// driver.findElement(By.id());
+	}
+
+	public void createLocalOrganisation(String arg1) {
+		// TODO Auto-generated method stub
+		login();
+
+		driver.findElement(By.id("nav-structure")).click();
+		driver.findElement(By.id("nav-structure-list-local")).click();
+		driver.findElement(By.cssSelector("span.button-content")).click();
+
+		driver.findElement(By.id("group-name")).clear();
+		driver.findElement(By.id("group-name")).sendKeys(arg1);
+
+		driver.findElement(
+				By.cssSelector("#save-localorg > span.ui-button-text > span.button-content"))
+				.click();
+
+	}
 }
--- src/test/java/at/ac/tuwien/inso/swtesten/lab/ValidAddMemberFormSubmitStepDefinitions.java
+++ src/test/java/at/ac/tuwien/inso/swtesten/lab/ValidAddMemberFormSubmitStepDefinitions.java
@@ -9,7 +9,7 @@ public class ValidAddMemberFormSubmitStepDefinitions {
 	
 	@Given("^I am on the sportsid homepage$")
 	public void I_am_on_the_sportsid_homepage() throws Throwable {
-	    // Express the Regexp above with the code you wish you had
+		// Express the Regexp above with the code you wish you had
 		helper.setUp();
 	}
 	
